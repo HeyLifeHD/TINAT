288e29aa1d3e7f9bbe42369c7bbd2a2e81eeca41
HeyLifeHD <joschkahey@gmail.com>
2023-05-17 14:20:45 +0200

    updated mismatch analyses

--- Scripts/human/RNAseq/H1299_ownData/deNovo_analysis/peptidomics/16.2_count_inducedORFs_occurance.r
+++ Scripts/human/RNAseq/H1299_ownData/deNovo_analysis/peptidomics/16.2_count_inducedORFs_occurance.r
@@ -100,7 +100,7 @@ gghistogram(number_of_MM_long[number_of_MM_long$Number_of_matches_with_pepteide_
 dev.off()
 
 number_of_MM$peptide <- rownames(number_of_MM)
-write.table(number_of_MM, file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_pepteide_sub_induced_compared_to_inducedORFs.txt"),row.names = FALSE, col.names=TRUE, quote=FALSE, sep=", ")
+write.table(number_of_MM, file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_pepteide_sub_induced_compared_to_inducedORFs.txt"),row.names = FALSE, col.names=TRUE, quote=FALSE, sep="\t")
 
 
 #generate detailed table
@@ -137,15 +137,15 @@ pattern_match_df <- rbind(pattern_match_noMM_df, pattern_match_1MM_df, pattern_m
 #pattern_match_df <- rbind(pattern_match_0MM_df, pattern_match_1MM_df, pattern_match_2MM_df, pattern_match_3MM_df)
 pattern_match_df$peptide <- sapply(strsplit(pattern_match_df$peptide, ".", fixed=TRUE), "[",1)
 
-write.table(pattern_match_df, file.path(output.dir, "2of3DACSB_unique", "peptide_blast_pepteide_sub_induced_compared_to_inducedORFs.txt"),row.names = FALSE, col.names=TRUE, quote=FALSE, sep=", ")
+write.table(pattern_match_df, file.path(output.dir, "2of3DACSB_unique", "peptide_blast_pepteide_sub_induced_compared_to_inducedORFs.txt"),row.names = FALSE, col.names=TRUE, quote=FALSE, sep="\t")
 
 
 
 #scp -r heyj@172.22.62.205:/omics/groups/OE0219/internal/tinat/integration/peptidomics/comparison_gene_expression/2of3DACSB_unique ./
 
 #Merge with transcript annotation table
-number_of_MM <- read.csv( file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_pepteide_sub_induced_compared_to_inducedORFs.txt"), header=TRUE)
-pattern_match_df <- read.csv(file.path(output.dir, "2of3DACSB_unique", "peptide_blast_pepteide_sub_induced_compared_to_inducedORFs.txt"),header=TRUE)
+number_of_MM <- read.delim( file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_pepteide_sub_induced_compared_to_inducedORFs.txt"), header=TRUE)
+pattern_match_df <- read.delim(file.path(output.dir, "2of3DACSB_unique", "peptide_blast_pepteide_sub_induced_compared_to_inducedORFs.txt"),header=TRUE)
 peptide_anno <- readxl::read_excel("/omics/groups/OE0219/internal/tinat/integration/peptidomics/data/220301_Peptide_Results_new.xlsx")
 ORF_anno <- read.delim(file.path("/omics/groups/OE0219/internal/tinat/integration/peptidomics/comparison_gene_expression", "manuscript_table.tsv"))
 anno <- readRDS("/omics/groups/OE0219/internal/tinat/210726_shortRead_processing_deNovo_custom4/gffCompare.annotated.sorted_repeat_anno.rds")
@@ -162,5 +162,147 @@ temp <- sapply(strsplit(as.character(pattern_match_df$names), ".p", fixed=TRUE),
 temp <- sapply(strsplit(temp, "ORF_", fixed=TRUE), "[", 2)
 pattern_match_df$transcript_id <- temp
 
-pattern_match_df_anno <- dplyr::left_join(pattern_match_df[,!colnames(pattern_match_df)%in% c("start", "end", "width")], as.data.frame(anno))
-write.table(pattern_match_df_anno, file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_pepteide_sub_induced_compared_to_inducedORFs_annotated.txt"),row.names = FALSE, col.names=TRUE, quote=FALSE, sep=", ")
+pattern_match_df_anno <- dplyr::left_join(pattern_match_df[,!colnames(pattern_match_df)%in% c("start", "end", "width")], as.data.frame(anno)[which( anno$type == "transcript"),])
+write.table(pattern_match_df_anno, file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_pepteide_sub_induced_compared_to_inducedORFs_annotated.txt"),row.names = FALSE, col.names=TRUE, quote=FALSE, sep="\t")
+
+#plot ERE annotation
+temp <-pattern_match_df_anno[pattern_match_df_anno$Mismatches_allowed =="0",]
+dim(temp)
+temp <- temp[which(temp$dist_nearest_LTR12repeat ==0),]
+dim(temp)
+table(temp$nearest_repeat_repName)
+pattern_match_df_anno$ERE_TSS_anno <- NA
+pattern_match_df_anno$ERE_TSS_anno <- ifelse(pattern_match_df_anno$dist_nearest_repeat == 0, pattern_match_df_anno$nearest_repeat_repClass, "no ERE")
+pattern_match_df_anno$ERE_TSS_anno <- ifelse(pattern_match_df_anno$ERE_TSS_anno %in% c("LTR", "LINE", "SINE", "no ERE"), pattern_match_df_anno$ERE_TSS_anno , "other")
+#split
+pattern_match_df_anno_split <- split(pattern_match_df_anno, pattern_match_df_anno$Mismatches_allowed)
+pattern_match_df_anno_split_stats <- lapply(pattern_match_df_anno_split, function(y){
+    y <- as.data.frame(table(y$ERE_TSS_anno))
+    colnames(y) <- c("repeat_class", "Freq")
+    y
+    })
+pie <- list()
+    for(j in names(pattern_match_df_anno_split_stats)){
+        labs <- paste0(pattern_match_df_anno_split_stats[[j]]$Freq)
+        pie[[j]] <- ggpie(pattern_match_df_anno_split_stats[[j]], "Freq", #label = labs,
+                lab.pos = "in", lab.font = "black", palette=ere_col,#label.size = 16,
+                fill = "repeat_class", color = "black")+rremove("legend.title")
+    }
+pie_eres <- ggarrange(pie[[1]], pie[[2]], pie[[3]],
+    labels=paste0(names(pie), " Mismatches"),
+    common.legend = TRUE,
+    ncol = 1, nrow = 3
+    )
+pdf( file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_pepteide_sub_induced_compared_to_inducedORFs_annotated_Pie_transcript_class_ERE_anno.pdf"), width=5, height=10)
+print(pie_eres)
+dev.off()
+
+
+#plot LTR annotation
+pattern_match_df_anno$LTR_TSS_anno <- NA
+pattern_match_df_anno$LTR_TSS_anno <- ifelse(pattern_match_df_anno$dist_nearest_LTR12repeat == 0, 
+    pattern_match_df_anno$nearest_LTR12repeat_repName, "no LTR12")
+#split
+pattern_match_df_anno_split <- split(pattern_match_df_anno, pattern_match_df_anno$Mismatches_allowed)
+pattern_match_df_anno_split_stats <- lapply(pattern_match_df_anno_split, function(y){
+    y <- as.data.frame(table(y$LTR_TSS_anno))
+    colnames(y) <- c("repeat_class", "Freq")
+    y
+    })
+pie <- list()
+    for(j in names(pattern_match_df_anno_split_stats)){
+        labs <- paste0(pattern_match_df_anno_split_stats[[j]]$Freq)
+        pie[[j]] <- ggpie(pattern_match_df_anno_split_stats[[j]], "Freq", #label = labs,
+                lab.pos = "in", lab.font = "black", palette=ltr_col,#label.size = 16,
+                fill = "repeat_class", color = "black")+rremove("legend.title")
+    }
+pie_eres <- ggarrange(pie[[1]], pie[[2]], pie[[3]],
+    labels=paste0(names(pie), " Mismatches"),
+    common.legend = TRUE,
+    ncol = 1, nrow = 3
+    )
+pdf( file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_pepteide_sub_induced_compared_to_inducedORFs_annotated_Pie_transcript_class_LTR_anno.pdf"), width=5, height=10)
+print(pie_eres)
+dev.off()
+
+
+
+#combined plotting with uniprot
+pattern_match_df_anno_1 <- read.delim(file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_uniprot_annotated.txt"))
+pattern_match_df_anno_2 <-  read.delim(file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_pepteide_sub_induced_compared_to_inducedORFs_annotated.txt"))
+
+pattern_match_df_anno <- do.call(
+    "rbind", list(pattern_match_df_anno_1[, c("Mismatches_allowed", "dist_nearest_repeat", "nearest_repeat_repClass","nearest_repeat_repName", "dist_nearest_LTR12repeat", "nearest_LTR12repeat_repName")], 
+    pattern_match_df_anno_2[, c("Mismatches_allowed",  "dist_nearest_repeat","nearest_repeat_repClass","nearest_repeat_repName",  "dist_nearest_LTR12repeat", "nearest_LTR12repeat_repName")] ))
+
+pattern_match_df_anno$dist_nearest_LTR12repeat <- gsub(" ", "",pattern_match_df_anno$dist_nearest_LTR12repeat)
+pattern_match_df_anno$dist_nearest_repeat <- gsub(" ", "",pattern_match_df_anno$dist_nearest_repeat)
+
+#library(dplyr)
+#pattern_match_df_anno <-pattern_match_df_anno %>%
+#  mutate_all(as.character)
+#plot ERE annotation
+temp <-pattern_match_df_anno[pattern_match_df_anno$Mismatches_allowed =="0",]
+dim(temp)
+temp <- temp[which(temp$dist_nearest_LTR12repeat ==0),]
+dim(temp)
+table(temp$nearest_repeat_repName)
+pattern_match_df_anno$ERE_TSS_anno <- NA
+pattern_match_df_anno$ERE_TSS_anno <- ifelse(pattern_match_df_anno$dist_nearest_repeat == 0, as.character(pattern_match_df_anno$nearest_repeat_repClass), "no ERE")
+pattern_match_df_anno$ERE_TSS_anno <- ifelse(pattern_match_df_anno$ERE_TSS_anno %in% c("LTR", "LINE", "SINE", "no ERE"), pattern_match_df_anno$ERE_TSS_anno , "other")
+#split
+pattern_match_df_anno_split <- split(pattern_match_df_anno, pattern_match_df_anno$Mismatches_allowed)
+pattern_match_df_anno_split_stats <- lapply(pattern_match_df_anno_split, function(y){
+    y <- as.data.frame(table(y$ERE_TSS_anno))
+    colnames(y) <- c("repeat_class", "Freq")
+    y
+    })
+pie <- list()
+    for(j in names(pattern_match_df_anno_split_stats)){
+        labs <- paste0(pattern_match_df_anno_split_stats[[j]]$Freq)
+        pie[[j]] <- ggpie(pattern_match_df_anno_split_stats[[j]], "Freq", #label = labs,
+                lab.pos = "in", lab.font = "black", palette=ere_col,#label.size = 16,
+                fill = "repeat_class", color = "black")+rremove("legend.title")
+    }
+pie_eres <- ggarrange(pie[[1]], pie[[2]], pie[[3]],
+    labels=paste0(names(pie), " Mismatches"),
+    common.legend = TRUE,
+    ncol = 1, nrow = 3
+    )
+pdf( file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_combined_annotated_Pie_transcript_class_ERE_anno.pdf"), width=5, height=10)
+print(pie_eres)
+dev.off()
+
+
+#plot LTR annotation
+pattern_match_df_anno$LTR_TSS_anno <- NA
+pattern_match_df_anno$LTR_TSS_anno <- ifelse(pattern_match_df_anno$dist_nearest_LTR12repeat == 0, 
+    as.character(pattern_match_df_anno$nearest_LTR12repeat_repName), "no LTR12")
+#split
+pattern_match_df_anno_split <- split(pattern_match_df_anno, pattern_match_df_anno$Mismatches_allowed)
+pattern_match_df_anno_split_stats <- lapply(pattern_match_df_anno_split, function(y){
+    y <- as.data.frame(table(y$LTR_TSS_anno))
+    colnames(y) <- c("repeat_class", "Freq")
+    y
+    })
+pie <- list()
+    for(j in names(pattern_match_df_anno_split_stats)){
+        labs <- paste0(pattern_match_df_anno_split_stats[[j]]$Freq)
+        pie[[j]] <- ggpie(pattern_match_df_anno_split_stats[[j]], "Freq", #label = labs,
+                lab.pos = "in", lab.font = "black", palette=ltr_col,#label.size = 16,
+                fill = "repeat_class", color = "black")+rremove("legend.title")
+    }
+pie_eres <- ggarrange(pie[[1]], pie[[2]], pie[[3]],
+    labels=paste0(names(pie), " Mismatches"),
+    common.legend = TRUE,
+    ncol = 1, nrow = 3
+    )
+pdf( file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_combined_annotated_Pie_transcript_class_LTR_anno.pdf"), width=5, height=10)
+print(pie_eres)
+dev.off()
+
+
+
+
+
+/omics/groups/OE0219/internal/tinat/integration/peptidomics/comparison_gene_expression/2of3DACSB_unique
--- Scripts/human/RNAseq/H1299_ownData/deNovo_analysis/peptidomics/16_count_UNIPROT_occurance.r
+++ Scripts/human/RNAseq/H1299_ownData/deNovo_analysis/peptidomics/16_count_UNIPROT_occurance.r
@@ -31,7 +31,6 @@ DEG_results_list<- readRDS(file.path(PostDE.dir, "DEG_results_group_list.rds"))
 repeats<- readRDS(file.path("c010-datasets/Internal/LTR/guideDesign/","Workspaces_David","repeats.RDS"))
 
 #get transcripts of anno
-anno_transcripts <- 
 #get color code
 library(RColorBrewer)
 col <- brewer.pal(12, "Paired")
@@ -135,15 +134,87 @@ pattern_match_3MM_df$peptide <- rownames(pattern_match_3MM_df)
 pattern_match_3MM_df$Mismatches_allowed <- "3"
 
 pattern_match_df <- rbind(pattern_match_noMM_df, pattern_match_1MM_df, pattern_match_2MM_df)
-#pattern_match_df <- rbind(pattern_match_0MM_df, pattern_match_1MM_df, pattern_match_2MM_df, pattern_match_3MM_df)
+#pattern_match_df <- rbind(pattern_match_noMM_df, pattern_match_1MM_df, pattern_match_2MM_df, pattern_match_3MM_df)
 pattern_match_df$peptide <- sapply(strsplit(pattern_match_df$peptide, ".", fixed=TRUE), "[",1)
 
-write.table(pattern_match_df, file.path(output.dir, "2of3DACSB_unique", "peptide_blast_uniprot.txt"),row.names = FALSE, col.names=TRUE, quote=FALSE, sep=", ")
+write.table(pattern_match_df, file.path(output.dir, "2of3DACSB_unique", "peptide_blast_uniprot.txt"),row.names = FALSE, col.names=TRUE, quote=FALSE, sep="\t")
 
 
-scp -r heyj@172.22.62.205:/omics/groups/OE0219/internal/tinat/integration/peptidomics/comparison_gene_expression/2of3DACSB_unique ./
+
+#Merge with transcript annotation table
+pattern_match_df <- read.table( file.path(output.dir, "2of3DACSB_unique", "peptide_blast_uniprot.txt"),header=TRUE, sep="\t")
+anno_original <-  readRDS("/omics/groups/OE0219/internal/tinat/raw_data_repo/references/gencode.v29lift37.annotation.repeat_anno.rds")
+temp <- sapply(strsplit(as.character(pattern_match_df$names), "|", fixed=TRUE), "[", 2)
+pattern_match_df$Protein <- temp
+#temp <- sapply(strsplit(as.character(pattern_match_df$names), "|", fixed=TRUE), "[", 3)
+#temp <- sapply(strsplit(temp, "_HUMAN", fixed=TRUE), "[", 1)
+evidence_anno <- artMS::artmsAnnotationUniprot(x = data.frame(Protein=temp),
+                                         columnid = 'Protein',
+                                         species = 'human')
+temp <- left_join(data.frame(Protein=temp), evidence_anno)                 
+#pattern_match_df$gene_name <- evidence_anno$Gene
+pattern_match_df <- left_join(pattern_match_df, temp)
+pattern_match_df$gene_name <- pattern_match_df$Gene
+pattern_match_df$gene_name[pattern_match_df$gene_name  %in% anno_original$gene_name]
+pattern_match_df$gene_name[!pattern_match_df$gene_name  %in% anno_original$gene_name]
+
+pattern_match_df_anno <- dplyr::left_join(pattern_match_df[,!colnames(pattern_match_df)%in% c("start", "end", "width")], as.data.frame(anno_original)[which( anno_original$type == "gene"),])
+write.table(pattern_match_df_anno, file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_uniprot_annotated.txt"),row.names = FALSE, col.names=TRUE, quote=FALSE, sep="\t")
+
+#plot ERE annotation
+pattern_match_df_anno$ERE_TSS_anno <- NA
+pattern_match_df_anno$ERE_TSS_anno <- ifelse(pattern_match_df_anno$dist_nearest_repeat == 0, pattern_match_df_anno$nearest_repeat_repClass, "no ERE")
+pattern_match_df_anno$ERE_TSS_anno <- ifelse(pattern_match_df_anno$ERE_TSS_anno %in% c("LTR", "LINE", "SINE", "no ERE"), pattern_match_df_anno$ERE_TSS_anno , "other")
+#split
+pattern_match_df_anno_split <- split(pattern_match_df_anno, pattern_match_df_anno$Mismatches_allowed)
+pattern_match_df_anno_split_stats <- lapply(pattern_match_df_anno_split, function(y){
+    y <- as.data.frame(table(y$ERE_TSS_anno))
+    colnames(y) <- c("repeat_class", "Freq")
+    y
+    })
+pie <- list()
+    for(j in names(pattern_match_df_anno_split_stats)){
+        labs <- paste0(pattern_match_df_anno_split_stats[[j]]$Freq)
+        pie[[j]] <- ggpie(pattern_match_df_anno_split_stats[[j]], "Freq", #label = labs,
+                lab.pos = "in", lab.font = "black", palette=ere_col,#label.size = 16,
+                fill = "repeat_class", color = "black")+rremove("legend.title")
+    }
+pie_eres <- ggarrange(pie[[1]], pie[[2]], 
+    labels=paste0(names(pie), " Mismatches"),
+    common.legend = TRUE,
+    ncol = 1, nrow = 3
+    )
+pdf( file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_uniprot_annotated_Pie_transcript_class_ERE_anno.pdf"), width=5, height=10)
+print(pie_eres)
+dev.off()
 
 
+#plot LTR annotation
+pattern_match_df_anno$LTR_TSS_anno <- NA
+pattern_match_df_anno$LTR_TSS_anno <- ifelse(pattern_match_df_anno$dist_nearest_LTR12repeat == 0, 
+    pattern_match_df_anno$nearest_LTR12repeat_repName, "no LTR12")
+#split
+pattern_match_df_anno_split <- split(pattern_match_df_anno, pattern_match_df_anno$Mismatches_allowed)
+pattern_match_df_anno_split_stats <- lapply(pattern_match_df_anno_split, function(y){
+    y <- as.data.frame(table(y$LTR_TSS_anno))
+    colnames(y) <- c("repeat_class", "Freq")
+    y
+    })
+pie <- list()
+    for(j in names(pattern_match_df_anno_split_stats)){
+        labs <- paste0(pattern_match_df_anno_split_stats[[j]]$Freq)
+        pie[[j]] <- ggpie(pattern_match_df_anno_split_stats[[j]], "Freq", #label = labs,
+                lab.pos = "in", lab.font = "black", palette=ltr_col,#label.size = 16,
+                fill = "repeat_class", color = "black")+rremove("legend.title")
+    }
+pie_eres <- ggarrange(pie[[1]], pie[[2]], 
+    labels=paste0(names(pie), " Mismatches"),
+    common.legend = TRUE,
+    ncol = 1, nrow = 3
+    )
+pdf( file.path(output.dir, "2of3DACSB_unique", "number_of_mismatches_uniprot_annotated_Pie_transcript_class_LTR_anno.pdf"), width=5, height=10)
+print(pie_eres)
+dev.off()
 
 
 
